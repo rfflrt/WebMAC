8f281d73395c79088e2a8d2fcf653767f0703d99
Raffael Raiél Trindade <raffaelraiel@usp.br>
2026-03-13 19:09:49 -0300

    Backend login post

--- aula5/main.py
+++ aula5/main.py
@@ -12,7 +12,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class Usuario(BaseModel):
     nome: str
-    #senha: str
+    senha: str
     bio: str
 
 usuarios_db = []
@@ -28,8 +28,32 @@ def criar_usuario(user: Usuario):
     usuarios_db.append(user.model_dump())
     return{"usuario": user.nome}
 
-@app.get("login")
+@app.get("/login")
 def get_login(request: Request):
     return templates.TemplateResponse(
         request=request, name="login.html"
     )
+
+@app.post("/login")
+def login(username: str, response: Response):
+    usuario_encontrado = None
+    for u in usuarios_db:
+        if u["nome"] == username:
+            usuario_encontrado = u
+            break
+
+    if not usuario_encontrado:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+    response.set_cookie(key="session_user", value = username)
+    return {"message": "Logado com sucesso"}
+
+def get_active_user(session_user: Annotated[str | None, Cookie()] = None):
+    if not session_user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Acesso negado: você não está logado")
+    
+    user = next((u for u in usuarios_db if u["nome"] == session_user), None)
+    if not user:
+        raise HTTPException(status_code=401, detail="Sessão inválida")
+    
+    return user
