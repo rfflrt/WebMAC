214d6f0229d147706789b16f96fe2a9da910ee16
Raffael Raiél Trindade <raffaelraiel@usp.br>
2026-03-19 16:35:56 -0300

    Exercícios de sala da aula 7

---
+++ aula7/api.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from sqlmodel import Session, select, SQLModel, create_engine
+from models import Aluno, Tarefa
+
+arquivo_sqlite = "exercicio_7.db"
+url_sqlite = f"sqlite:///{arquivo_sqlite}"
+
+engine = create_engine(url_sqlite)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup() -> None:
+    create_db_and_tables()
+
+@app.post("/alunos")
+def criar_aluno(aluno: Aluno):
+    with Session(engine) as session:
+        session.add(aluno)
+        session.commit()
+        session.refresh(aluno)
+        return aluno
+
+@app.post("/tarefas")
+def criar_tarefa(tarefa: Tarefa):
+    with Session(engine) as session:
+        session.add(tarefa)
+        session.commit()
+        session.refresh(tarefa)
+        return tarefa
+
+
+@app.get("/alunos")
+def listar_alunos():
+    with Session(engine) as session:
+        return session.exec(select(Aluno)).all()
+
+
+@app.get("/tarefas")
+def listar_tarefas():
+    with Session(engine) as session:
+        return session.exec(select(Tarefa)).all()
+
+@app.get("/alunos/{aluno_nusp}/tarefas")
+def listar_tarefas_do_aluno(aluno_nusp: int):
+    with Session(engine) as session:
+        return(session.exec(select(Aluno).where(Aluno.nusp == aluno_nusp)).first().tarefas)
